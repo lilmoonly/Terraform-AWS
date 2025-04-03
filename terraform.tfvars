def99abc7d5f9ee969f56a211c4cf59a7845dacb
lilmoonly <bohdan.muliarchyk@lnu.edu.ua>
2025-04-03 15:09:18 +0000

    fix var

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,11 @@
 region       = "eu-central-1"
 project_name = "forgejo"
+
+eks_managed_node_groups = {
+  default = {
+    instance_types = ["t3.micro"]
+    desired_size   = 1
+    min_size       = 1
+    max_size       = 1
+  }
+}
--- variables.tf
+++ variables.tf
@@ -17,3 +17,13 @@ variable "cluster_name" {
 variable "cluster_version" {
   default = "1.29"
 }
+
+variable "eks_managed_node_groups" {
+  description = "Map of EKS managed node group configurations"
+  type = map(object({
+    instance_types = list(string)
+    desired_size   = number
+    min_size       = number
+    max_size       = number
+  }))
+}
